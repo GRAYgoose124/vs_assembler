7fe34c22282e70c1fb174e70d0fb971e9ae5cf47
Grayson Miller <grayson.miller124@gmail.com>
2023-02-23 11:35:34 -0700

    CodeLauncherJson

--- scripts/set_launch_module.py
+++ scripts/set_launch_module.py
@@ -5,7 +5,7 @@ import argparse
 import os
 
 
-DEFAULT_LAUNCH = {
+DEFAULT_LAUNCH_DICT = {
     "version": "0.2.0",
     "configurations": [
         {
@@ -25,69 +25,102 @@ DEFAULT_LAUNCH = {
 }
 
 
-def build_launch_configuration(module=None, env_root=None, env_name=None, python=None):
-    """ Build the launch configuration for the given module. """
- 
-    launch = deepcopy(DEFAULT_LAUNCH)
-    if module:
-        launch['configurations'][0]['module'] = module
+class CodeLaunchJson:
+    """ This class is an OOP implementation of the functions above. """
+    def __init__(self, json_path):
+        self.json_path = json_path
+        self._launch = None
 
-        print(f"Using module: {launch['configurations'][0]['module']}")
+        self.load(json_path)
 
-    if env_root and env_name and python:
-        if env_root.startswith("~"):
-            env_root = os.path.expanduser(env_root)
-            
-        env = f"{env_root}/.{env_name}"
+    def load(self, json_path=None):
+        """ Update the json path and load the json. """
+        json_path = self.__check_json_path(json_path)
 
-        launch['configurations'][0]['pythonPath'] = f"{env}/bin/{python}"
-        launch['configurations'][0]['env']['PYTHONPATH'] = f"{env}/lib/{python}/site-packages"
+        with open(json_path, 'r') as f:
+            self._launch = json.load(f)
 
-        print(f"Using python path: {launch['configurations'][0]['pythonPath']}")
+        return self
 
-    return launch
+    def save(self, json_path=None):
+        """ Save the launch object to the launch.json file. """
+        json_path = self.__check_json_path(json_path)
 
+        with open(json_path, 'w') as f:
+            json.dump(self._launch, f, indent=2)
 
-def load_launch_json(launch_json):
-    """ Open the launch.json file and return the json object. """
-    with open(launch_json, 'r') as f:
-        return json.load(f)
+    def diff(self, other):
+        """
+            See DEFAULT_LAUNCH above for an example of the format VSCode uses.
 
+            This function will compare the two launch dicts and yield the differences.
+        """ 
+        other = self.__check_other(other)                
 
-def save_launch_json(launch_json, launch):
-    """ Save the launch object to the launch.json file. """
-    with open(launch_json, 'w') as f:
-        json.dump(launch, f, indent=2)
+        for key, value in self._launch['configurations'][0].items():
+            if other['configurations'][0][key] != value:
 
+                yield key, (value, other['configurations'][0][key])
 
-def diff_launches(one, two):
-    """
-        See DEFAULT_LAUNCH above for an example of the format VSCode uses.
+    def update_keys(self, **kwargs):
+        """ Update the launch.json with the given kwargs. """
+        for key, value in kwargs.items():
+            if key in self._launch['configurations'][0]:
+                self._launch['configurations'][0][key] = value
+        
+        return self
 
-        This function will compare the two launch dicts and yield the differences.
-    """ 
-    for key, value in one['configurations'][0].items():
-        if two['configurations'][0][key] != value:
-            yield key, (value, two['configurations'][0][key])
+    def update(self, other):
+        """ Update the launch.json with the values from other. """
+        other = self.__check_other(other)
 
+        self.update_keys(**other['configurations'][0])
 
-def update_launch_module(launch_json, module):
-    """ Update the launch.json to use the given module. """
-    
-    launch = load_launch_json(launch_json)
-    launch['configurations'][0]['module'] = module
-    with open(launch_json, 'w') as f:
-        json.dump(launch, f, indent=2)
+        return self
 
+    def build(self, module=None, env_root=None, env_name=None, python=None):
+        """ Build the launch configuration for the given module. """
+        self._launch = deepcopy(DEFAULT_LAUNCH_DICT)
+        if module:
+            self._launch['configurations'][0]['module'] = module
 
-def update_config_key(launch_json, key, value):
-    """ Update the launch.json to use the given module. """
-    
-    launch = load_launch_json(launch_json)
-    launch['configurations'][0][key] = value
-    with open(launch_json, 'w') as f:
-        json.dump(launch, f, indent=2)
+            print(f"Using module: {self._launch['configurations'][0]['module']}")
 
+        if env_root and env_name and python:
+            if env_root.startswith("~"):
+                env_root = os.path.expanduser(env_root)
+                
+            env = f"{env_root}/.{env_name}"
+
+            self._launch['configurations'][0]['pythonPath'] = f"{env}/bin/{python}"
+            self._launch['configurations'][0]['env']['PYTHONPATH'] = f"{env}/lib/{python}/site-packages"
+
+            print(f"Using python path: {self._launch['configurations'][0]['pythonPath']}")
+
+        return self
+
+    def __check_other(self, other):
+        if isinstance(other, CodeLaunchJson):
+            return other._launch
+        elif not isinstance(other, dict):
+            raise TypeError("other must be a dict or CodeLaunchJson object.")
+        elif 'configurations' not in other:
+            raise KeyError("other must have a 'configurations' key.")
+        
+        return other
+
+    def __check_json_path(self, json_path):
+        if json_path is None:
+            json_path = self.json_path
+        
+        if json_path is None:
+            raise ValueError("json_path must be set.")
+
+        return json_path
+
+    def __str__(self) -> str:
+        # print launch dict pretty
+        return json.dumps(self._launch, indent=2)
 
 def argparser():
     # lets define an argparser that lets us change the launch module easily and generate a new .vscode/ + launch.json
@@ -131,31 +164,36 @@ def main():
         # if the user defaults are disabled, we just set the config file
         args.config = args.config or ".vscode/launch.json"
 
+    # If the config file is not an absolute path, we need to make it one
+    # if not os.path.isabs(args.config):
+    #     args.config = os.path.join(os.getcwd(), args.config)
+
     # Get current launch.json
     try:
-        launch = load_launch_json(args.config)
+        launch = CodeLaunchJson(args.config)
     except FileNotFoundError:
         launch = None
 
     # Create new launch.json
-    new_launch = build_launch_configuration(args.module, args.env_root, args.env_name, args.python)
-    diffs = list(diff_launches(launch, new_launch))
+    new_launch = CodeLaunchJson(args.config).build(module=args.module, env_root=args.env_root, env_name=args.env_name, python=args.python)
+    diffs = list(new_launch.diff(launch))
 
     # If there is no launch.json, we simply need to create one
     if launch is None:
-        save_launch_json(args.config, new_launch)
+        new_launch.save()
     else:
         # If there is one, but there are no differences, we don't need to do anything
         # otherwise, we need to update the launch.json and save it.
         if len(diffs):
-            launch['configurations'][0].update(new_launch['configurations'][0])
+            launch.update(new_launch)
+            launch.save()
 
             # Show the differences
             for key, (default, current) in diffs:
                 print(f"\t{key}: {default} -> {current}")
 
-            # Save the new launch.json
-            save_launch_json(args.config, launch)
+          
+            
 
 if __name__ == "__main__":
     main()
